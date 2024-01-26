4b909e7dcc2f204092764e6a794a633cc04d883f
Zoey <joyivan6@gmail.com>
2024-01-27 01:02:05 +0700

    feat: add basic controllers

--- controllers/post.controller.go
+++ controllers/post.controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"facepedia-backend-service/models"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -19,4 +21,35 @@ func (pc *PostController) GetPost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	postID := vars["ID"]
 
+	var post models.Post
+	if err := pc.DB.First(&post, postID).Error; err != nil {
+		http.Error(w, "Post not found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Data: %+v", post)
+}
+
+func (pc *PostController) UpdatePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postID := vars["ID"]
+
+	if err := pc.DB.Model(&models.Post{}).Where("ID = ?", postID).Update(r.Form).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Post has been udpated")
+}
+
+func (pc *PostController) DeletePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postID := vars["ID"]
+
+	if err := pc.DB.Delete(&models.Post{}, postID).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Post has been deleted")
 }
--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,1 +1,43 @@
 package controllers
+
+import (
+	"facepedia-backend-service/models"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+type UserController struct {
+	DB *gorm.DB
+}
+
+func NewUserController(db *gorm.DB) *UserController {
+	return &UserController{DB: db}
+}
+
+func (uc *UserController) GetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := vars["ID"]
+
+	var user models.User
+	if err := uc.DB.First(&user, userID).Error; err != nil {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Data: %+v", user)
+}
+
+func (uc *UserController) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := vars["ID"]
+
+	if err := uc.DB.Model(&models.Post{}).Where("ID = ?", userID).Update(r.Form).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "User has been updated")
+}
